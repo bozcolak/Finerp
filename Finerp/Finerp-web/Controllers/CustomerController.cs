801352318bf39ff2d2e3d68eb58a21acc58dc36f
bozcolak <burakozcolak@gmail.com>
2023-06-20 13:22:45 +0300

    Customer List added

--- Finerp/Finerp-web/Controllers/CustomerController.cs
+++ Finerp/Finerp-web/Controllers/CustomerController.cs
@@ -17,4 +17,41 @@ public class CustomerController : Controller
         var customers = _customerRepository.GetAllCustomers();
         return View(customers);
     }
+
+    public IActionResult Detail(int customerId)
+    {
+        Customer customer = new Customer();
+        if (customerId != 0)
+            customer = _customerRepository.GetCustomerById(customerId);
+        return View(customer);
+    }
+
+    [HttpPost]
+    public IActionResult Detail(Customer customer)
+    {
+        if (customer.CustomerId == 0)
+        {
+            _customerRepository.AddCustomer(customer);
+        }
+        else
+        {
+            _customerRepository.UpdateCustomer(customer);
+        }
+
+
+        return
+            RedirectToAction(
+                "Index"); // İşlem tamamlandıktan sonra yönlendirme yapılabilir, örneğin Index sayfasına yönlendirme yapılıyor.
+    }
+
+    [HttpPost]
+    public IActionResult Delete(Customer customer)
+    {
+        _customerRepository.DeleteCustomer(customer.CustomerId);
+
+
+        return
+            RedirectToAction(
+                "Index"); // İşlem tamamlandıktan sonra yönlendirme yapılabilir, örneğin Index sayfasına yönlendirme yapılıyor.
+    }
 }
--- Finerp/Finerp-web/Program.cs
+++ Finerp/Finerp-web/Program.cs
@@ -44,4 +44,6 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
+
+
 app.Run();
